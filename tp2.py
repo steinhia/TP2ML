3ca517db07f268d4a9c85c36839ee5103825e3a3
Alexandra Steinhilber <alexandra.steinhilber@grenoble-inp.org>
2017-12-15 13:58:37 +0100

    ajout analyse erreurs

--- tp2.py
+++ tp2.py
@@ -6,8 +6,7 @@ import cv2
 from matplotlib import pyplot as plt
 from createGlobalEllipseDataBase import Ellipse
 from createGlobalEllipseDataBase import loadGlobalListFromFile
-
-
+from os import path
 
 
 
@@ -15,10 +14,12 @@ def testScaleFactor(ellipseDict):
     minNeighbors=3
     L=[]
     dico=dict()
-    for i in np.arange(1.05,1.7,0.1):
+    for i in np.arange(1.05,1.2,0.1):
         rate=testDataBase(i,minNeighbors,ellipseDict)
         dico[i]=rate
     return dico
+
+
 
 def testMinNeighbors(ellipseDict):
     scaleFactor=1.2
@@ -31,8 +32,12 @@ def testMinNeighbors(ellipseDict):
 
 def testDataBase(scaleFactor,minNeighbors,ellipseDict):
     rate=[0,0,0]
+    rateLittle=[0,0,0]
     i=0
+    rateBig=[0,0,0]
     n_images=0
+    n_imagesLittle=0
+    n_imagesBig=0
     for ellipses in ellipseDict:
         i+=1
         if(i<100):
@@ -54,14 +59,30 @@ def testDataBase(scaleFactor,minNeighbors,ellipseDict):
                 F1=2*float(anal[0])/float(2*anal[0]+anal[2]+anal[1])              
                 anal=[TPR,PPV,F1]
                 rate=[rate[j]+anal[j] for j in range(3)]
+		if(len(ellipseDict[ellipseFilename])<4):
+                	rateLittle=[rateLittle[j]+anal[j] for j in range(3)]
+			n_imagesLittle+=1
+    		if(len(ellipseDict[ellipseFilename])>3):
+                	rateBig=[rateBig[j]+anal[j] for j in range(3)]
+			n_imagesBig+=1
     rate=[rate[j]/n_images for j in range(3)]
-    print str(n_images) + " images lues"
-    return rate
+    rateBig=[rateBig[j]/n_imagesBig for j in range(3)]
+    rateLittle=[rateLittle[j]/n_imagesLittle for j in range(3)]
+#    print str(n_images) + " images lues"
+#    print "rate"
+#    print rate
+#    print "rateLittle"
+#    print rateLittle
+#    print "big"
+#    print rateBig
+    return [rateLittle,rateBig,rate]
             
 def UnitTest(scaleFactor,minNeighbors,ellipseFilename,trace):
-    face_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
+    face_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_alt2.xml')
     #eye_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_eye.xml')
+    profil_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_profilface.xml')
     path="originalPics/" + ellipseFilename + ".jpg"
+    #path=ellipseFilename
     img = cv2.imread(path)
     res=[]
     #if img is None:
@@ -72,16 +93,26 @@ def UnitTest(scaleFactor,minNeighbors,ellipseFilename,trace):
         #print path
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         faces = face_cascade.detectMultiScale(gray, scaleFactor, minNeighbors)
+        profiles = profil_cascade.detectMultiScale(gray, scaleFactor, minNeighbors)
         res=[]
         for (x,y,w,h) in faces:
             if(trace):
+		print "face"
                 cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
             else:
                 res.append([x+w/2,y+h/2])
+	for (x,y,w,h) in profiles:
+            if(trace):
+		print "profil"	
+                cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+            else:
+                res.append([x+w/2,y+h/2])
+        
         if(trace):
-            cv2.imshow('img',img)
-            cv2.waitKey(0)
-            cv2.destroyAllWindows()
+        	cv2.imshow('img',img)
+        	cv2.imwrite(ellipseFilename+"_detect.jpg",img)
+        	cv2.waitKey(0)
+        	cv2.destroyAllWindows()
     else:
         res=-1
     return res
@@ -100,11 +131,25 @@ def  analyseResult(res,ellipses):
                 TP+=1
     FN=max(len(ellipses)-TP,0)
     FP=max(len(res)-TP,0)
+    if(float(TP)/float(len(ellipses))<1 and len(ellipses)>5):
+	print TP
+	print FN
+        for ellipse in ellipses:
+		print ellipse
     return [TP,FN,FP]
-
 ellipseList = loadGlobalListFromFile("ellipseList.pkl")
-#UnitTest(1.2,5,"2002/08/02/big/img_838",True)
+
+
+
+
+
+#UnitTest(1.2,5,"images_unitaires/profil2.jpg",True)
+#UnitTest(1.2,5,"2002/07/19/big/img_576",True)
+
 print "TP,FN,FP"
 scaleFactor=1.2
 minNeighbors=3
 print(testScaleFactor(ellipseList))
+
+
+
